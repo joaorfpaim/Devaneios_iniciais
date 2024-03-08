6e2914c4f0bb883e0e8cd02282fe9cc70ab20b19
João Paim <joaoeter11@gmail.com>
2024-03-07 21:45:09 -0300

    Create quizz1.py

---
+++ quizz1.py
@@ -0,0 +1,10 @@
+print('1- Quantos albums o Matuê tem?\na) 1\nb) 2\nc) 3\nd) 4')
+p1 = input('Resposta:').lower()
+
+if p1 not in ['a','b','c','d']:
+    print('Error')
+else:
+    if p1 == 'a':
+        print('Correct answer.')
+    else:
+        print('Incorrect answer.')
